504c1538715c63a9bf827ff574a00af57e08eb61
Hassan Yamchi <Hassan.Yamchi@encana.com>
2023-02-08 13:37:56 -0700

    configured bastion host and bastion service

---
+++ 12-Azure-Bastion-Service-and-Host/terraform-manifests/c8-03-move-ssh-key-to-bastion-host.tf
@@ -0,0 +1,40 @@
+# Create a Null Resource and Provisioners
+resource "null_resource" "null_copy_ssh_key_to_bastion" {
+  depends_on = [
+    azurerm_linux_virtual_machine.bastion_host_linuxvm
+  ]
+
+  # Connection Block for Provisioners to connect to Azure VM Instance
+  connection {
+    type        = "ssh"
+    host        = azurerm_linux_virtual_machine.bastion_host_linuxvm.public_ip_address
+    user        = azurerm_linux_virtual_machine.bastion_host_linuxvm.admin_username
+    private_key = file("${path.module}/ssh-keys/terraform-azure.pem")
+  }
+
+  ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
+  provisioner "file" {
+    source      = "ssh-keys/terraform-azure.pem"
+    destination = "/tmp/terraform-azure.pem"
+    # when = create or destroy >> default is create
+    # on_failure = fail or continue >> default is fail
+  }
+
+  ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 400 /tmp/terraform-azure.pem"
+    ]
+  }
+}
+
+# Creation Time Provisioners - By default they are created during resource creations (terraform apply)
+# Destory Time Provisioners - Will be executed during "terraform destroy" command (when = destroy)
+
+
+
+
+
+
+
+
